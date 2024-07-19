66545b50cc3c3f135c22e7c6e199c736c9fba29f
Yash Jangid <yashcx84@gmail.com>
2024-07-19 20:52:40 +0530

    .

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+  };
+  
+  module.exports = nextConfig;
+  
